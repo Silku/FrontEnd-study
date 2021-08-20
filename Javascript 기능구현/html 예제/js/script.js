c25bf0d33e400faf5cae56a91c95600169d5b84a
김태우 <kokicola@naver.com>
2021-08-20 17:38:54 +0900

    21.08.20 슬라이더 및 클론코딩

--- Javascript 기능구현/html 예제/js/script.js
+++ Javascript 기능구현/html 예제/js/script.js
@@ -1,99 +1,132 @@
-//--메뉴--
-$(function () {
-  $('.s_mn > li  ').hide();
-
-  $('.m_mn > li').mouseenter(function () {
-    $(this).find('.s_mn > li ').slideDown();
-    /*
-    $(this).find('s_mn).show();
-    $(this).find('s_mn).fadeIn();
-    $(this).find('s_mn).animate();
-    $(this).find('s_mn).slideDown();
-    */
-  });
-  $('.m_mn > li').mouseleave(function () {
-    $(this).find('.s_mn > li ').slideUp();
-  });
-});
-
-//--메뉴 호버--
-$('.m_mn > li').hover(
-  function () {
-    $(this).css({ color: '#aa3344' });
-  },
-  function () {
-    $(this).css({ color: '#red' });
-  }
-);
-
-//--슬라이드
-var current = 0;
-var setIntervalId;
 $(document).ready(function () {
-  var current = 0;
+  //--메뉴--
+  $(function () {
+    $('.s_mn > li  ').hide();
 
-  $('btns > li').click(function (e) {
+    $('.m_mn > li').mouseenter(function () {
+      $(this).find('.s_mn > li ').slideDown();
+      /*
+        $(this).find('s_mn).show();
+        $(this).find('s_mn).fadeIn();
+        $(this).find('s_mn).animate();
+        $(this).find('s_mn).slideDown();
+        */
+    });
+    $('.m_mn > li').mouseleave(function () {
+      $(this).find('.s_mn > li ').slideUp();
+    });
+  });
+
+  //--메뉴 호버--
+  $('.m_mn > li').hover(
+    function () {
+      $(this).css({ color: '#aa3344' });
+    },
+    function () {
+      $(this).css({ color: '#red' });
+    }
+  );
+
+  // --메인슬라이드
+
+  let current = 0;
+  let setIntervalId;
+
+  function move(i) {
+    var currentEl = $('.imgs > li').eq(current);
+    var nextEl = $('.imgs > li').eq(i);
+
+    currentEl.css({ left: '0%' }).animate({ left: '-100%' });
+    nextEl.css({ left: '100%' }).animate({ left: '0%' });
+
+    current = i;
+  }
+
+  function move_return(i) {
+    var currentEl = $('.imgs > li').eq(current);
+    var nextEl = $('.imgs > li').eq(i);
+
+    currentEl.css({ left: '0%' }).animate({ left: '100%' });
+    nextEl.css({ left: '-100%' }).animate({ left: '0%' });
+    current = i;
+  }
+
+  timer();
+  function timer() {
+    // setInterval(실행함수, 시간)
+    //clearInterval(setInterval 적용된 함수)
+
+    setIntervalId = setInterval(function () {
+      var n = current + 1;
+      if (n > 2) {
+        n = 0;
+      }
+      move(n);
+    }, 1500);
+  }
+
+  $('#main_img').mouseover(function () {
+    clearInterval(setIntervalId);
+  });
+  $('#main_img').mouseout(function () {
+    timer();
+  });
+
+  $('.btns > li').click(function (e) {
     e.preventDefault();
-    var i = $(this).index();
+    let i = $(this).index();
+    // console.log(i);
     move(i);
   });
-});
 
-$('.l_btn').click(function () {
-  var l = current - 1;
-  if (l == 3) {
-    l = 0;
-  }
-  move(l);
-});
-
-$('.r_btn').click(function () {
-  var r = current + 1;
-  if (r == 3) {
-    r = 0;
-  }
-  move_return(r);
-});
-
-$('#main_img').on({
-  mouseover: function () {
-    clearInterval(setIntervalId);
-  },
-  mouseout: function () {
-    timer();
-  },
-});
-
-// 자동전환
-timer();
-function timer() {
-  setIntervalId = setInterval(function () {
-    var n = current + 1;
-    if (n == 3) {
-      n = 0;
+  $('.l_btn').click(function () {
+    let l = current - 1;
+    if (l > 2) {
+      l = 0;
     }
-    move(n);
-  }, 2000);
-}
+    move(l);
+  });
+  $('.r_btn').click(function () {
+    let r = current + 1;
+    if (r > 2) {
+      r = 0;
+    }
+    move_return(r);
+  });
+  // ---퀵메뉴 트레이싱
+  var box = $('#all > .box');
 
-function move(i) {
-  if (current == i) return;
-  var currentEl = $('.imgs> li').eq(current);
-  var nextEl = $('.imgs> li').eq(i);
+  $('#q_mn li').click(function () {
+    let qbt = $(this).index();
+    let currentbox = box.eq(qbt);
+    let tg = currentbox.offset().top;
+    /*
+    position();부모값
+    offset(); 자기
+    console.log(tg);
+    */
+    $('html,body').stop().animate({ scrollTop: tg });
+  });
 
-  currentEl.css({ left: '0%' }).animate({ left: '-100%' });
-  nextEl.css({ left: '100%' }).animate({ left: '0%' });
+  $(window).scroll(function () {
+    var sct = $(window).scrollTop() + 200;
+    $('#q_mn')
+      .stop()
+      .animate({ top: sct + 'px' }, 600);
+  });
 
-  current = i;
-}
+  // --미니슬라이드;
+  // $('.con01_btns > li').click(function () {
+  //   var num = $(this).index();
 
-function move_return(i) {
-  if (current == i) return;
-  var currentEl = $('.imgs > li').eq(current);
-  var nextEl = $('.imgs > li').eq(i);
+  //   slide_img(num);
+  // });
+  // function slide_img(num) {
+  //   posX = num * 360 + 'px';
+  //   $('.in_box > ul').animate({ left: posX }, 600);
+  // }
 
-  currentEl.css({ left: '0%' }).animate({ left: '100%' });
-  nextEl.css({ left: '-100%' }).animate({ left: '0%' });
-
-  current = i;
-}
+  $('.con05').mouseover(function () {
+    $('.con05').animate({ width: '300px' });
+  });
+});
---
+++ Javascript 기능구현/html 예제/js/vanilla.js
@@ -0,0 +1,30 @@
+// 작은컨테이너(con1) 1-4번 기능
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll('.in_box > ul > li');
+  let dots = document.querySelectorAll('.con01_btns > li');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
