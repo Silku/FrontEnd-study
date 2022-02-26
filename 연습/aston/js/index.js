b2c723dc443df8b669396b0aa2a5f3c8b7cc5b79
DESKTOP-94T42NM\Admin <kokicola@naver.com>
2022-02-26 20:56:28 +0900

    220226 https://astoncm.com/ 클론코딩

---
+++ 연습/aston/js/index.js
@@ -0,0 +1,30 @@
+window.addEventListener("wheel", function(e){
+    e.preventDefault();
+},{passive : false});
+
+var mHtml = $("html");
+var page = 1;
+mHtml.animate({scrollTop : 0}, 1000);
+
+
+
+
+$(window).on("wheel", function(e) {
+    if(mHtml.is(":animated")) return;
+    if(e.originalEvent.deltaY > 0) {
+    // deltaY > 0 휠을 아래로 스크롤할때
+        if(page == 5){
+            page = 0;
+            return
+        }
+        // return;
+        page++;
+    }else if(e.originalEvent.deltaY < 0) {
+    // deltaY < 0 휠을 위로 스크롤할때
+        if(page == 1) return;
+        page--;
+    }
+    // 페이지 높이만큼 스크롤
+    var posTop =(page-1) * $(window).height();
+    mHtml.animate({scrollTop : posTop});
+})
