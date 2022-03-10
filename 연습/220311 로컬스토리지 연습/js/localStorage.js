0d4afdc1503f930d52c90582fc73d769c7fa1982
김태우 <kokicola@naver.com>
2022-03-11 00:23:28 +0900

    22.03.11 로컬스토리지 연습 시작

---
+++ 연습/220311 로컬스토리지 연습/js/localStorage.js
@@ -0,0 +1,7 @@
+// 브라우저 로컬 데이터에 저장하기
+
+// [ ! ] 주요 메서드 3개
+// 값 저장하기 --> localStorage.setItem( 'key', value );
+// 값 가져오기 --> localStorage.getItem( 'key' );
+// 값 삭제하기 --> localStorage.removeItem( 'key' );
+// 키(key)를 그대로 놓고 새로운 값을 저장하면 덮어쓰기가 되면서 기존 로컬 데이터 정보가 수정.
