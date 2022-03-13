f383e9bd6b6bafe5946e05424f23237cc1e43ea4
김태우 <kokicola@naver.com>
2022-03-13 22:54:22 +0900

    22.03.11 로컬스토리지 getItem, setItem, removeItem, clear()

--- 연습/220311 로컬스토리지 연습/js/localStorage.js
+++ 연습/220311 로컬스토리지 연습/js/localStorage.js
@@ -5,3 +5,38 @@
 // 값 가져오기 --> localStorage.getItem( 'key' );
 // 값 삭제하기 --> localStorage.removeItem( 'key' );
 // 키(key)를 그대로 놓고 새로운 값을 저장하면 덮어쓰기가 되면서 기존 로컬 데이터 정보가 수정.
+
+window.onkeydown = function (e) {
+  //   console.log('물리 키다운');
+  // console.log(e.keyCode);
+};
+
+window.onload = () => {
+  const btnSetLocalData = document.querySelector('.btnSetLocalData');
+  const btnGetLocalData = document.querySelector('.btnGetLocalData');
+  const btnRemoveLocalData = document.querySelector('.btnRemoveLocalData');
+  const input = document.querySelector('input');
+  const input2 = document.querySelector('.input2');
+
+  btnSetLocalData.addEventListener('click', function () {
+    let inputValue = input.value;
+    console.log(inputValue);
+    localStorage.setItem('id', inputValue);
+  });
+
+  btnGetLocalData.addEventListener('click', function () {
+    let getData = localStorage.getItem('id');
+    if (!getData) {
+      alert('저장된 데이터가 없습니다.');
+    } else {
+      input2.value = getData;
+    }
+  });
+
+  btnRemoveLocalData.addEventListener('click', function () {
+    // localStorage.removeItem('id');
+    localStorage.clear();
+    input.value = '';
+    input2.value = '';
+  });
+};
