10cd3431516330df3e83bccf03b92c157b2f4eec
김태우 <kokicola@naver.com>
2022-03-27 11:59:01 +0900

    22.03.27 버튼 동적생성 데이터 할당

--- 연습/220322 설문조사 연습/js/start.js
+++ 연습/220322 설문조사 연습/js/start.js
@@ -1,6 +1,36 @@
 const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 qna.style.display = 'none';
+
+function addAnswer(answerText, qIdx) {
+  var a = document.querySelector('.answer_box');
+  let answer = document.createElement('Button');
+  answer.classList.add('answerList');
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+
+  answer.addEventListener(
+    'click',
+    function () {
+      let children = document.querySelectorAll('.answerList');
+      for (let i = 0; i < children.length; i++) {
+        children[i].disabled = true;
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    },
+    false
+  );
+}
+
+function goNext(qIdx) {
+  var q = document.querySelector('.q_box');
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+}
+
 begin = () => {
   main.style.WebkitAnimation = 'fadeOut 1s';
   main.style.animation = 'fadeOut 1s';
@@ -12,5 +42,7 @@ begin = () => {
       main.style.display = 'none';
       qna.style.display = 'block';
     }, 450);
+    let qIdx = 0;
+    goNext(qIdx);
   }, 450);
 };
