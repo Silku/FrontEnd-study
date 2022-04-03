8081b09a92d26aebdfb976260c0352ca75247a79
김태우 <kokicola@naver.com>
2022-04-03 21:05:41 +0900

    22.04.03 설문조사 만들기/ 프로그레스바, gradient

--- 연습/220322 설문조사 연습/js/start.js
+++ 연습/220322 설문조사 연습/js/start.js
@@ -1,7 +1,24 @@
 const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
+const result = document.querySelector('#result');
+const endPoint = 12;
 qna.style.display = 'none';
 
+function goResult() {
+  qna.style.WebkitAnimation = 'fadeOut 1s';
+  qna.style.animation = 'fadeOut 1s';
+
+  setTimeout(() => {
+    result.style.WebkitAnimation = 'fadeIn 1s';
+    result.style.animation = 'fadeIn 1s';
+    setTimeout(() => {
+      qna.style.display = 'none';
+      result.style.display = 'block';
+    }, 450);
+    let qIdx = 0;
+    goNext(qIdx);
+  }, 450);
+}
 function addAnswer(answerText, qIdx) {
   var a = document.querySelector('.answerBox');
   let answer = document.createElement('Button');
@@ -9,7 +26,7 @@ function addAnswer(answerText, qIdx) {
   answer.classList.add('my-3');
   answer.classList.add('py-3');
   answer.classList.add('mx-auto');
-  answer.classList.add('fadeIn')
+  answer.classList.add('fadeIn');
   a.appendChild(answer);
   answer.innerHTML = answerText;
 
@@ -21,23 +38,29 @@ function addAnswer(answerText, qIdx) {
         children[i].disabled = true;
         children[i].style.animation = 'fadeOut 0.5s';
       }
-      setTimeout(()=>{
+      setTimeout(() => {
         for (let i = 0; i < children.length; i++) {
           children[i].style.display = 'none';
         }
         goNext(++qIdx);
-      },450)
+      }, 450);
     },
     false
   );
 }
 
 function goNext(qIdx) {
+  if (qIdx + 1 === endPoint) {
+    goResult();
+    return;
+  }
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for (let i in qnaList[qIdx].a) {
     addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
+  let status = document.querySelector('.statusBar');
+  status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
 }
 
 begin = () => {
