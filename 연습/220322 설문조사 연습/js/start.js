a91bae767fbcd51e03c28b39652f2c17603fd43e
김태우 <kokicola@naver.com>
2022-04-04 01:10:09 +0900

    22.04.04 설문조사 만들기/ result 알고리즘 1

--- 연습/220322 설문조사 연습/js/start.js
+++ 연습/220322 설문조사 연습/js/start.js
@@ -2,7 +2,49 @@ const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 const result = document.querySelector('#result');
 const endPoint = 12;
-qna.style.display = 'none';
+const select = [];
+
+function calResult() {
+  let pointArray = [
+    { name: 'mouse', value: 0, key: '0' },
+    { name: 'cow', value: 0, key: '1' },
+    { name: 'tiger', value: 0, key: '2' },
+    { name: 'rabbit', value: 0, key: '3' },
+    { name: 'dragon', value: 0, key: '4' },
+    { name: 'snake', value: 0, key: '5' },
+    { name: 'horse', value: 0, key: '6' },
+    { name: 'sheep', value: 0, key: '7' },
+    { name: 'monkey', value: 0, key: '8' },
+    { name: 'chicken', value: 0, key: '9' },
+    { name: 'dog', value: 0, key: '10' },
+    { name: 'pig', value: 0, key: '11' },
+  ];
+  for (let i = 0; i < endPoint; i++) {
+    var target = qnaList[i].a[select[i]];
+    for (let j = 0; j < target.type.length; j++) {
+      for (let k = 0; k < pointArray.length; k++) {
+        if (target.type[j] === pointArray[k].name) {
+          pointArray[k].value += 1;
+        }
+      }
+    }
+  }
+  // sort compareFunction(a,b)
+  // => 0을 기준으로 0보다 작다면 a가 먼저옴
+  // => 0을 기준으로 0보다 크다면 b가 먼저옴
+  var resultArray = pointArray.sort(function (a, b) {
+    if (a.value > b.value) {
+      return -1;
+    }
+    if (a.value < b.value) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log(resultArray);
+  let resultword = resultArray[0].key;
+  return resultword;
+}
 
 function goResult() {
   qna.style.WebkitAnimation = 'fadeOut 1s';
@@ -18,8 +60,10 @@ function goResult() {
     let qIdx = 0;
     goNext(qIdx);
   }, 450);
+  console.log(select);
+  calResult();
 }
-function addAnswer(answerText, qIdx) {
+function addAnswer(answerText, qIdx, idx) {
   var a = document.querySelector('.answerBox');
   let answer = document.createElement('Button');
   answer.classList.add('answerList');
@@ -39,6 +83,7 @@ function addAnswer(answerText, qIdx) {
         children[i].style.animation = 'fadeOut 0.5s';
       }
       setTimeout(() => {
+        select[qIdx] = idx;
         for (let i = 0; i < children.length; i++) {
           children[i].style.display = 'none';
         }
@@ -50,14 +95,14 @@ function addAnswer(answerText, qIdx) {
 }
 
 function goNext(qIdx) {
-  if (qIdx + 1 === endPoint) {
+  if (qIdx === endPoint) {
     goResult();
     return;
   }
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for (let i in qnaList[qIdx].a) {
-    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
   let status = document.querySelector('.statusBar');
   status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
