3d14bf43303562e6a82cec6492d62e6e8ea0d15d
김태우 <kokicola@naver.com>
2022-05-03 22:19:33 +0900

    Revert "220503 node.js 연습, nodeMailer, smtp 써서 form 형태로 메일보내기"
    This reverts commit 6e9fb7098d2b6a3a4fda8711aef819470cc6ad0d.

--- 연습/nodejs prc/20_nodemailer.js
+++ 연습/nodejs prc/20_nodemailer.js
@@ -4,7 +4,7 @@ const transPorter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
     user: 'b99348799@gmail.com',
-    pass: 'kim66496!!',
+    pass: '1234',
   },
   host: 'smtp',
   port: '465',
@@ -12,16 +12,15 @@ const transPorter = nodemailer.createTransport({
 
 const mailOption = {
   from: '김태우<b99348799@gmail.com>',
-  to: '김태우<kokicola@naver.com>',
+  to: '김태우<b99348799@gmail.com>',
   subject: 'node.js로 메일보내기 테스트',
-  html: "<h2>node.js로 보내는메일 테스트</h2><p style='font-size : 15px; color: deeppink;'>테스트 메세지!!</p>",
+  html: "<h2>node.js로 보내는메일 테스트</h2><p style='font-size : 15px; color: deeppink;>테스트 메세지!!</p>",
 };
 
 transPorter.sendMail(mailOption, (err, info) => {
   if (err) {
     console.log(err);
   } else {
-    res.status(200).send(`<h2>메일을 성공적으로 발송했습니다.♥</h2>`);
     console.log(info);
   }
   transPorter.close();
