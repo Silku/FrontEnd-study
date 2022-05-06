d974266bec4bde2e18a442074d43cde24fa4a480
김태우 <kokicola@naver.com>
2022-05-06 14:34:05 +0900

    220506 node.js 연습, JSON 연습

--- 연습/nodejs prc/22_module.js
+++ 연습/nodejs prc/22_module.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const module1 = require('./router/module1')(app);
+const module1 = require('./router/module1')(app, fs);
 
 app.listen(port, () => {
   console.log(`${port}포트로 서버 실행 중...`);
--- 연습/nodejs prc/router/module1.js
+++ 연습/nodejs prc/router/module1.js
@@ -1,6 +1,6 @@
 // const express = require('express');
 // const app = express();
-module.exports = (app) => {
+module.exports = (app, fs) => {
   app.get('/', (req, res) => {
     console.log('module1.js 실행상태');
     res.render('index.ejs', {
@@ -12,5 +12,30 @@ module.exports = (app) => {
   app.get('/about', (req, res) => {
     // views/about.html
     res.render('about.html');
+  });
+
+  // http://127.0.0.1:3000/list
+  app.get('/list', (req, res) => {
+    fs.readFile(__dirname + '/../data/member.json', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+        res.writeHead(200, { 'content-type': 'text/json;charset=utf-8' });
+        res.end(data);
+      }
+    });
+  });
+  // http://127.0.0.1:3000/getMember/banana
+  // : 콜론쓰면 변수로 취급할수 있음
+  app.get('/getMember/:userid', (req, res) => {
+    fs.readFile(__dirname + '/../data/member.json', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const member = JSON.parse(data);
+        res.json(member[req.params.userid]);
+      }
+    });
   });
 };
