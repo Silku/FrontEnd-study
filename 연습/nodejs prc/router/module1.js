897189f7ae66e9a42930e8e223be3649b4836c64
김태우 <kokicola@naver.com>
2022-05-07 13:41:04 +0900

    220507 node.js 연습, JSON파일 읽기 쓰기

--- 연습/nodejs prc/router/module1.js
+++ 연습/nodejs prc/router/module1.js
@@ -38,4 +38,45 @@ module.exports = (app, fs) => {
       }
     });
   });
+
+  // http://127.0.0.1:3000/joinMember/avocado
+  app.post('/joinMember/:userid', (req, res) => {
+    const result = {};
+    const userid = req.params.userid;
+    if (!req.body['password'] || !req.body['name']) {
+      result['code'] = 100; //100:실패
+      result['msg'] = '매개변수를 찾을수 없습니다.';
+      res.json(result);
+      return false;
+    }
+    fs.readFile(__dirname + '/../data/member.json', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const member = JSON.parse(data);
+        if (member[userid]) {
+          result['code'] = 101;
+          result['msg'] = '중복된 아이디';
+          res.json(result);
+          return false;
+        }
+        console.log(req.body);
+        member[userid] = req.body;
+        fs.writeFile(
+          __dirname + '/../data/member.json',
+          JSON.stringify(member, null, '\t'),
+          'utf-8',
+          (err, data) => {
+            if (err) {
+              console.log(err);
+            } else {
+              result['code'] = 200;
+              result['msg'] = '성공';
+              res.json(result);
+            }
+          }
+        );
+      }
+    });
+  });
 };
