9dc2fb64a9c536f0dc4a3f550d431afe1120d977
김태우 <kokicola@naver.com>
2022-05-30 20:28:03 +0900

    220530 express, mongodb를 이용한 회원가입 만들기1

--- 연습/nodejs prc/26_mongodb.js
+++ 연습/nodejs prc/26_mongodb.js
@@ -12,6 +12,70 @@ let database;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//회원가입 만들기 - http://127.0.0.1:3000/member/regist (post)
+router.route('/member/regist').post((req, res) => {
+  console.log('/member/regist 호출됨!');
+  const userid = req.body.userid;
+  const userpw = req.body.userpw;
+  const name = req.body.name;
+  const gender = req.body.gender;
+  console.log(
+    `userid:${userid}, userpw:${userpw}, name:${name}, gender:${gender}`
+  );
+  if (database) {
+    joinMember(database, userid, userpw, name, gender, (err, result) => {
+      if (err) {
+        res.writeHead('200', { 'content-type': 'text/html;charset=utf-8' });
+        res.write('<h2>회원가입 실패!</h2>');
+        res.write('<h2>오류가 발생했습니다.</h2>');
+        res.end();
+      } else {
+        if (result.insertCount > 0) {
+          res.writeHead('200', { 'content-type': 'text/html;charset=utf-8' });
+          res.write('<h2>회원가입 성공!</h2>');
+          res.write('<h2>가입이 성공적으로 완료되었습니다.</h2>');
+          res.end();
+        } else {
+          res.writeHead('200', { 'content-type': 'text/html;charset=utf-8' });
+          res.write('<h2>회원가입 실패!</h2>');
+          res.write('<h2>회원가입에 실패하였습니다.</h2>');
+          res.end();
+        }
+      }
+    });
+  } else {
+    res.writeHead('200', { 'content-type': 'text/html;charset=utf-8' });
+    res.write('<h2>데이터베이스 연결 실패!</h2>');
+    res.write('<h2>mongodb 데이터 베이스 연결 안됨!</h2>');
+    res.end();
+  }
+});
+
+const joinMember = function (database, userid, userpw, name, gender, callback) {
+  console.log('joinMember 호출됨.');
+  const members = database.collection('member');
+  //insertMany = members 컬렉션으로 가져온 데이터를 객체로 저장하게 해줄수 있는 메소드
+  members.insertMany(
+    [{ userid: userid, userpw: userpw, name: name, gender: gender }],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        callback(err, null);
+        return;
+      } else {
+        if (result.insertedCount > 0) {
+          console.log(
+            `사용자 document ${result.insertedCount}명이 추가 되었습니다.`
+          );
+        } else {
+          console.log(`사용자 document가 추가되지 않았습니다.`);
+        }
+        callback(null, result);
+      }
+    }
+  );
+};
+
 //몽고 db 연결 함수
 function connectDB() {
   const databaseURL = 'mongodb://localhost:27017';
