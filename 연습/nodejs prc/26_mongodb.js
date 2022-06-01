b273e283b421d923a21c7b63c482166e813d387e
김태우 <kokicola@naver.com>
2022-06-01 22:40:38 +0900

    220601 express, mongodb를 이용한 로그인 만들기(post)

--- 연습/nodejs prc/26_mongodb.js
+++ 연습/nodejs prc/26_mongodb.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const e = require('express');
 const MongoClient = require('mongodb').MongoClient;
 //mongodb의 MongoClient 클래스 객체를 사용
 
@@ -51,6 +52,49 @@ router.route('/member/regist').post((req, res) => {
   }
 });
 
+//로그인 만들기 - http://127.0.0.1:3000/member/login (post)
+router.route('/member/login').post((req, res) => {
+  console.log('/member/login 호출!');
+  const userid = req.body.userid;
+  const userpw = req.body.userpw;
+  console.log(`userid:${userid}, userpw:${userpw}`);
+  if (database) {
+    loginMember(database, userid, userpw, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.writeHead('200', { 'content-type': 'text/html;charset=utf-8' });
+        res.write('<h2>로그인 실패!</h2>');
+        res.write('<h2>오류가 발생했습니다.</h2>');
+        res.end();
+      } else {
+        if (result) {
+          const result_userid = result[0].userid;
+          const result_userpw = result[0].userpw;
+          const result_name = result[0].name;
+          const result_gender = result[0].gender;
+          res.writeHead('200', { 'content-type': 'text/html;charset=utf-8' });
+          res.write('<h2>로그인 성공!</h2>');
+          res.write(`<p>아이디 : ${result_userid}</p>`);
+          res.write(`<p>패스워드 : ${result_userpw}</p>`);
+          res.write(`<p>이름 : ${result_name}</p>`);
+          res.write(`<p>성별 : ${result_gender}</p>`);
+          res.end();
+        } else {
+          res.writeHead('200', { 'content-type': 'text/html;charset=utf-8' });
+          res.write('<h2>로그인 실패!</h2>');
+          res.write('<p>아이디 또는 비밀번호를 확인하세요.</p>');
+          res.end();
+        }
+      }
+    });
+  } else {
+    res.writeHead('200', { 'content-type': 'text/html;charset=utf-8' });
+    res.write('<h2>데이터베이스 연결 실패!</h2>');
+    res.write('<h2>mongodb 데이터 베이스 연결 안됨!</h2>');
+    res.end();
+  }
+});
+
 const joinMember = function (database, userid, userpw, name, gender, callback) {
   console.log('joinMember 호출됨.');
   const members = database.collection('member');
@@ -74,6 +118,27 @@ const joinMember = function (database, userid, userpw, name, gender, callback) {
       }
     }
   );
+};
+
+const loginMember = function (database, userid, userpw, callback) {
+  console.log('loginMember 호출!');
+  const members = database.collection('member');
+
+  members.find({ userid: userid, userpw: userpw }).toArray((err, result) => {
+    if (err) {
+      console.log(err);
+      callback(err, null);
+      return;
+    } else {
+      if (result.length > 0) {
+        console.log('사용자를 찾았습니다.');
+        callback(null, result);
+      } else {
+        console.log('일치하는 사용자가 없습니다.');
+        callback(null, null);
+      }
+    }
+  });
 };
 
 //몽고 db 연결 함수
