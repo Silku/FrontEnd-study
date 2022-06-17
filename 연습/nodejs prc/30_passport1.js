9b23908da676cfe172f9eecbb6d4f478d565c17f
김태우 <kokicola@naver.com>
2022-06-17 23:58:03 +0900

    220616 passport모듈 1

---
+++ 연습/nodejs prc/30_passport1.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const static = require('serve-static');
+const path = requrie('path');
+const expressErrorHandler = require('express-error-handler');
+//npm i passport
+const passport = require('passport');
+
+const app = express();
+const port = 3000;
+const router = express.Router();
+
+app.use(cookieParser());
+app.use(
+  expressSession({
+    secret: '!@#$%^&*()',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 },
+  })
+);
+
+app.use(logger('dev'));
+app.use(passport.initialize()); //패스포트 초기화
+//passport의 세션을 사용하려면 반드시 express의 세션코드가 먼저 설정되어야함
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/public', static(path.join(__dirname, 'public')));
+
+app.use('/', router);
+
+const errorHandler = expressErrorHandler({
+  static: {
+    404: './public/404.html',
+  },
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+app.listen(port, () => {
+  console.log(`${port}포트로 서버 실행중... `);
+  connectDB();
+});
