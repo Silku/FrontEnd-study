2556607ffd1674b08933cac1d7a8cb72c8577cfc
김태우 <kokicola@naver.com>
2022-07-04 22:26:45 +0900

    220704 react todolist-3, navbar추가(props)

--- 연습/react prc/todolist/src/app.jsx
+++ 연습/react prc/todolist/src/app.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, {Component}from 'react';
 import './app.css';
+import NavBar from './components/navbar.jsx';
 import Todos from './components/todos';
 
+class App extends Component{
+
+    state = {
+        todos: [
+            {id: 1, name:'study', count:0},
+            {id: 2, name:'breath', count:0},
+            {id: 3, name:'stand', count:0},
+        ]
+    };
+
+    handleIncrement = todo =>{
+        const todos = [...this.state.todos];
+        const index = todos.indexOf(todo);
+        todos[index].count++;
+        this.setState({todos})
+    }
+
+    handleDecrement = todo =>{
+        const todos = [...this.state.todos];
+        const index = todos.indexOf(todo);
+        const count = todos[index].count-1; 
+        todos[index].count = count < 0 ? 0 : count;
+        this.setState({todos})
+    }
+
+    handleDelete = todo =>{
+        const todos = this.state.todos.filter(item => item.id !== todo.id)
+        this.setState({todos});
+    }
+
+    render(){
+        return(
+            <>
+                <NavBar totalCount={this.state.todos.filter(item => item.count > 0).length}/>
+                <Todos
+                    todos={this.state.todos}
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecrement}
+                    onDelete={this.handleDelete}
+                />
+            </>
+        )
+    }
+
+}
 
 
-function App() {
-    return (
-        <div className="todo">
-            <Todos/>
-        </div>
-    );
-} 
 
 export default App;
---
+++ 연습/react prc/todolist/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class NavBar extends Component{
+    render(){
+        return(
+        <div className="navbar">
+            <i className="fa-solid fa-leaf navbar-logo"></i>
+            <span>오늘의 할일 목록</span>
+            <span className="navbar-count">{this.props.totalCount}</span>
+        </div>
+        )   
+    }
+}
+
+
+
+export default NavBar;
