be6c09f95b295711e687ca02bbe7e677e5ce9cb7
김태우 <kokicola@naver.com>
2022-07-10 22:59:46 +0900

    220710 scss컴포넌트 디자인(classnames 모듈, @mixin 등)

--- 연습/react prc/test2/src/App.js
+++ 연습/react prc/test2/src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import Counter from './Counter';
 import Button from './components/button';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className='App'>
         <Counter/>
         <div className='buttons'>
-             <Button>클릭하기 </Button>
+				<Button size='large'>클릭하기 </Button>
+				<Button >클릭하기 </Button>
+				<Button size='small'>클릭하기 </Button>
         </div>
-    </>
+		<div className='buttons' >
+				<Button size='large' color='gray'>클릭하기 </Button>
+				<Button  color='gray'>클릭하기 </Button>
+				<Button size='small' color='gray'>클릭하기 </Button>
+        </div>
+		<div className='buttons' >
+				<Button size='large' color='violet'>클릭하기 </Button>
+				<Button  color='violet'>클릭하기 </Button>
+				<Button size='small' color='violet'>클릭하기 </Button>
+        </div>
+		<div className='buttons' >
+				<Button size='large' color='pink'  outline>클릭하기 </Button>
+				<Button  color='gray' outline>클릭하기 </Button>
+				<Button size='small' color='violet' outline>클릭하기 </Button>
+        </div>
+		<div className='buttons' >
+				<Button  color='pink'  fullWidth>클릭하기 </Button>
+				<Button  color='gray' fullWidth>클릭하기 </Button>
+				<Button  color='violet' fullWidth>클릭하기 </Button>
+        </div>
+    </div>
 
   );
 }
--- 연습/react prc/test2/src/components/button.js
+++ 연습/react prc/test2/src/components/button.js
@@ -1,8 +1,20 @@
 import React from "react";
+import classNames from  'classnames'
 import './button.scss';
 
-function Button({children}){
-    return <button className="Button">{children}</button>
+
+//props로 넘기기 (size)
+function Button({children , size, color, outline, fullWidth}){
+    // return 이렇게도 가능  (classname를 쓰기 이전)
+    // return <button className={['Button', size, color, outline].join(' ')}>{children}</button>
+    // return <button className={`Button ${size} ${color} ${outline}`}>{children}</button>
+    return <button className={classNames('Button', size, color, {outline, fullWidth})}>{children}</button>
+
+}
+
+Button.defaultProps = {
+    size : 'medium',
+    color : 'pink'
 }
 
 export default Button;
