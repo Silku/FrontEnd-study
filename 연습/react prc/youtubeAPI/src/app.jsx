194f3d0c1b081344b835eb3a215536377ed9f3b4
김태우 <kokicola@naver.com>
2022-07-17 22:33:16 +0900

    220717 youtubeAPI 사용하기-1, postman

---
+++ 연습/react prc/youtubeAPI/src/app.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+	const [videos, setVideos] = useState([])
+	useEffect(()=>{
+		const requestOptions = {
+			method: 'GET',
+			redirect: 'follow'
+			};
+			
+			fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&maxResult=20&chart=mostPopular&key=AIzaSyAmAA8pD07oj5RG0J3OURiqQvlz917W-Fk", requestOptions)
+			.then(response => response.text())
+			.then(result => setVideos(result.item))
+			.catch(error => console.log('error', error));
+	},[])
+	return (
+		<VideoList videos={videos}/>
+	)
+}
+
+export default App;
