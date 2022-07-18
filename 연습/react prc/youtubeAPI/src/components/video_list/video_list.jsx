fcc6364925a985e8da09a2aa0aaa05a7010a8f0f
김태우 <kokicola@naver.com>
2022-07-18 16:39:20 +0900

    220718 youtubeAPI 사용하기-2 post css

--- 연습/react prc/youtubeAPI/src/components/video_list/video_list.jsx
+++ 연습/react prc/youtubeAPI/src/components/video_list/video_list.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import VideoItem from "../video_item/video_item";
-
+import styles from './video_list.module.css'
 const VideoList = props => (
-    <ul>
+    <ul className={styles.videos}>
         {props.videos.map(video => (
             <VideoItem video={video} key={video.id}/>
             //console key값에러 -> key값으로 id추가
