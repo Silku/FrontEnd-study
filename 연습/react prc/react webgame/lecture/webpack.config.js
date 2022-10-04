c8fb2aa1167b0c424e49b6022e8a19d5d0c660cd
김태우 <kokicola@naver.com>
2022-10-04 22:25:56 +0900

    노드로 웹팩 설정하기 2(빌드)

--- 연습/react prc/react webgame/lecture/webpack.config.js
+++ 연습/react prc/react webgame/lecture/webpack.config.js
@@ -7,16 +7,20 @@ module.exports = {
     name: 'wordrelay',
     mode: 'development', // 실서비스: production
     devtool: 'eval', // 실서비스: hidden-source-map
+    //resolve : entry에 등록된 파일명에 해당하는 확장자를 웹팩에서 자동으로 찾아줌
+    resolve : {
+        extensions : ['.js', '.jsx']
+    },
 
     // entry : 입력하는 부분
     entry : {
-        app : ['./client.jsx', './WordRelay.jsx'],
+        app : './client',
     },
     // output : 출력하는 부분
     output : {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js',
     },
-
+ 
 };
 
