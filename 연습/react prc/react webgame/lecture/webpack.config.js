963734998cd7cedb8737808ea57a6a41a3566d32
김태우 <kokicola@naver.com>
2022-10-13 23:50:23 +0900

    노드로 웹팩 설정하기 4(preset-env)

--- 연습/react prc/react webgame/lecture/webpack.config.js
+++ 연습/react prc/react webgame/lecture/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 // path 조작을 도와주는 node
 
 
+//웹팩 설정  entry, module, plugins, output + mode 등 기타설정 흐름 잘 기억하기!
 
 module.exports = {
     name: 'wordrelay',
@@ -17,16 +18,32 @@ module.exports = {
         app : './client',
     },
     
+    /**
+    presets : preset-env는 환경설정을 해주는 모듈. 배열형태로 해서 세부설정을 해줄수 있음.
+    한국에서 브라우저점유율 5%이상, 최신 크롬 버전 -2 브라우저 까지 동작하도록 설정한 상태
+
+    브라우저 지원 관련 참조 링크
+    https://github.com/browserslist/browserslist
+    **/
     module:{
         rules :[{
             test : /\.jsx?/,
             loader : 'babel-loader',
             options : {
-                presets:['@babel/preset-env', '@babel/preset-react'],
+                presets:[
+                    ['@babel/preset-env', {
+                        targets:{
+                            browsers:['> 5% in KR', 'last 2 chrome versions']
+                        },
+                        debug: true,
+                    }], 
+                    '@babel/preset-react'],
             }
         }]
     },
-
+    //plugins 기본적으로 babel에서 지원하는것 외에 더 붙이고 싶을때 사용
+    plugins: [
+    ],
     // output : 출력하는 부분
     output : {
         path: path.join(__dirname, 'dist'),
