f7ad56d2d05419af332da1d71b897e94289f8dec
김태우 <kokicola@naver.com>
2022-10-14 23:48:16 +0900

    끝말잇기 만들기

--- 연습/react prc/react webgame/lecture/WordRelay.jsx
+++ 연습/react prc/react webgame/lecture/WordRelay.jsx
@@ -3,10 +3,48 @@ const {Component} = React;
 
 class WordRelay extends Component{
     state = {
-        text : 'Hello, webpack'
+        word: '서대문',
+        value : '',
+        result : '',
     };
+
+    onSubmitForm = (e) =>{
+        e.preventDefault();
+        if(this.state.word[this.state.word.length - 1] === this.state.value[0]){
+            this.setState({
+                result:'정답',
+                word:this.state.value,
+                value:'',
+
+            })
+            this.input.focus();
+        }else{
+            this.setState({
+                result:'불일치',
+                value:'',
+            })
+        }
+    }
+
+    onRefInput = (c) =>{
+        this.input = c;
+    }
+
+    onChangeInput = (e) =>{
+        this.setState({value : e.target.value})
+    }
+
     render(){
-        return <h1>{this.state.text}</h1>
+        return (
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput } value={this.state.value} onChange={this.onChangeInput}/>
+                    <button>입력!</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        )
     }
 }
 
