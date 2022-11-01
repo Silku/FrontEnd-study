62c8123e6d3209dc904e3ee107e6121cba9edcec
김태우 <kokicola@naver.com>
2022-11-01 15:35:59 +0900

    22.11.01 vue 로또추첨기

---
+++ 연습/react prc/react webgame/99.vuePractice/6.로또추첨기/LottoBall.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="ball" :style="styleObj">{{number}}</div>
+</template>
+
+<script lang="ts">
+    export default{
+        name:'LottoBall',
+        props:{
+            number:Number,
+        },
+        // !!금지사항 : props로 받은 데이터를 아래에서 바꾸는 행위
+        /**
+            ex/
+            methods:{
+                changeNumber(){
+                    this.number = 5;
+                }
+            }
+         **/ 
+        computed:{
+            styleObj(){
+                let background;
+                    if(this.number <=10){
+                        background='red'
+                    }else if(this.number <=20){
+                        background='orange'
+                    }else if(this.number <=30){
+                        background='yellow'
+                    }else if(this.number <=40){
+                        background='blue'
+                    }else{
+                        background='green'
+                    }
+                return{
+                background,
+                };
+            }
+        },
+        }
+</script>
+<style>
+    .ball{
+        display: inline-block;
+        border:1px solid black;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        font-size: 20px;
+        text-align: center;
+        margin-right: 20px;
+        color:white;
+    }
+</style>
--- 연습/react prc/react webgame/99.vuePractice/6.로또추첨기/LottoGenerator.vue
+++ 연습/react prc/react webgame/99.vuePractice/6.로또추첨기/LottoGenerator.vue
@@ -1,24 +1,72 @@
 
 <template>
     <div>
-
+        <div>당첨숫자</div>
+        <div id="결과창"> 
+            <LottoBall v-for="ball in winBalls" v-bind:key="ball" v-bind:number="ball"></LottoBall>
+        </div>
+        <div>보너스</div>
+            <LottoBall v-if="bonus" :number="bonus"></LottoBall>
+        <button v-if="redo" @click="onClickRedo">한번 더!</button>
     </div>
 </template>
 
 
 <script>
+import LottoBall from './LottoBall.vue';
 
+    function getWinNumbers(){
+        console.log('getWinNubers');
+        const candidate = Array(45).fill().map((v,i) => i +1);
+        const shuffle = [];
+        while(candidate.length > 0){
+            shuffle.push(candidate.splice(Math.floor(Math.random()*candidate.length),1)[0]);
+        }
+        const bonusNumber = shuffle[shuffle.length -1];
+        const winNumbers = shuffle.slice(0,6).sort((p,c) => p-c);
+        return [...winNumbers, bonusNumber];
+    }
+
+    const timeouts = [] 
     export default {
+        // LottoBall이라는 자식컴포넌트를 불러오기 위해서 components를 써줘야함.
+        components: {
+            LottoBall
+            // 이름을 다르게 할 경우 아래와 같이 사용도 가능
+            // 'lotto-ball':LottoBall.vue
+        },
         data(){
             return{
-
+                winNumbers:getWinNumbers(),
+                winBalls:[],
+                bonus:null,
+                redo:false
             }
         },
         computed:{
 
         },
         methods:{
-
+            onClickRedo(){
+                this.winNumbers = getWinNumbers();
+                this.winBalls= [];
+                this.bonus=null;
+                this.redo = false;
+                this.showBalls();
+            },
+            showBalls(){
+                 //6자리 숫자 표시
+                for(let i = 0; i < this.winNumbers.length - 1; i++){
+                    timeouts[i] = setTimeout(()=>{
+                        this.winBalls.push(this.winNumbers[i]);
+                    }, (i+1)*1000);
+                }
+                // 보너스 숫자 표시
+                    timeouts[6] = setTimeout(()=>{
+                        this.bonus = this.winNumbers[6];
+                        this.redo= true;
+                    },7000)
+                }
         },
         beforeCreate(){
             console.log('beforeCreate')
@@ -28,10 +76,14 @@
         },
         mounted(){
             console.log('mounted')
-            this.changeHand();
+            // 화면이 뜨자마자 mounted 되므로 mounted에 공이 나타나는 타이머에 대한 코드작성
+            this.showBalls();
         },
         beforeDestroy(){
-
+            // 예를들어 화면이 중간에 사라졌는데 setTimeout이 계속 있다면 메모리 누수가 예상되므로 라이프사이클 마지막에 반드시 클리어해줘야됨
+            timeouts.forEach((t) =>{
+                clearTimeout(t);
+            })
         },
         destroyed(){
             console.log('destroyed')
@@ -45,9 +97,9 @@
 
 <style scoped>
 
-*{
+/* *{
     margin: 0 auto;
     text-align: center;
-}
+} */
 
 </style>
