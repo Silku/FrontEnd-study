a39b2c48d8dd178af05ee18c078b3a80d2321987
김태우 <kokicola@naver.com>
2023-03-22 23:53:39 +0900

    practice : OpenWeatherAPI fix

--- nomad/chromeApp/js/weather.js
+++ nomad/chromeApp/js/weather.js
@@ -6,9 +6,13 @@ const weather = document.querySelector('.weather tbody td:nth-child(2)')
 const temp = document.querySelector('.weather tbody td:nth-child(3)')
 const wind = document.querySelector('.weather tbody td:nth-child(4)')
 
-// const API_KEY = API_KEY
+const degToCompass = (num) => {
+    const val = Math.floor((num / 22.5) + 0.5);
+    const arr = ['북', '북북동', '동북동', '동동북', '동', '동동남', '남동', '남남동', '남', '남남서', '서남서', '서서남', '서', '서북서', '북서', '북북서'];
+    return arr[(val % 16)];
+}
 
-const onGeoOk= (position) =>{
+const callbackOk= (position) =>{
     const lat = position.coords.latitude //위도
     const lon = position.coords.longitude //경도
     const lang = 'kr' //언어
@@ -19,22 +23,21 @@ const onGeoOk= (position) =>{
     fetch(url)
         .then(response => response.json())
         .then(data=>{
-
+            const temperature = Math.round(data.main.temp)
+            const windDirection = degToCompass(data.wind.deg)
             city.innerText = data.name
             weather.innerText = data.weather[0].description
-            temp.innerText = `${data.main.temp}도`
-            wind.innerText = `${data.wind.deg}/${data.wind.speed}`
-            // console.log(data.name)
-            // console.log(data.weather[0].main)
+            temp.innerText = `${temperature}도`
+            wind.innerText = `${windDirection} ${data.wind.speed}m/s`
         })
-    }
+}
 
-const onGeoError= () =>{
+const callbackError= () =>{
     alert("위치정보를 찾을 수 없습니다.")
 }
 
 // 사용자의 현재 위치정보를 가져옴
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError) //getCurrentPosition(successCallback, errorCallback, options)
+navigator.geolocation.getCurrentPosition(callbackOk, callbackError) //getCurrentPosition(successCallback, errorCallback, options)
 
 
 
