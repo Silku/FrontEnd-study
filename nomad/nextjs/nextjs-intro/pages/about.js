51848b7615e31894fe4cf18d2817d6f4c9be9aba
김태우 <kokicola@naver.com>
2023-04-14 21:59:09 +0900

    practice : LayoutPattern, next/head

--- nomad/nextjs/nextjs-intro/pages/about.js
+++ nomad/nextjs/nextjs-intro/pages/about.js
@@ -1,8 +1,9 @@
-import Navbar from "@/components/Navbar";
+import SEO from "@/components/SEO";
 
 export default function About(){
     return (
         <>
+            <SEO title="About"/>
             <p>어바웃 페이지 ㅋㅋㅎ</p>
         </>
     )
--- nomad/nextjs/nextjs-intro/pages/index.js
+++ nomad/nextjs/nextjs-intro/pages/index.js
@@ -1,19 +1,18 @@
-import Navbar from "@/components/Navbar";
+import SEO from "@/components/SEO";
+import Head from "next/head";
 import { useState } from "react";
+
+
 
 export default function Home(){
 	const [counter, setCounter]= useState(0);
 
 	return(
 		<>
+			<SEO title="Home"/>
 			<h1>Hello</h1>
 			<div>카운터 : {counter}</div>
 			<button type="button" onClick={()=>{setCounter((prev)=>prev+1)}}>+</button>
-			<style jsx global>{`
-				a{
-					color:red
-				}
-			`}</style>
 		</>
 	)
 }
